eb53a6159f9271667abaeb4a354f5b77a62adc2c
yezhan <yezhanyz@gmail.com>
2025-09-03 12:54:42 +0200

    modified:   implementation/src/experiment.rs 	modified:   implementation/src/main.rs

--- implementation/src/main.rs
+++ implementation/src/main.rs
@@ -7,7 +7,7 @@ mod dataset;
 fn main() {
     // 创建一个 x个节点的 Chord 环，id 空间大小为 2^m
     // Create a Chord ring with x nodes, and an ID space of size 2^m
-    let mut net = network::Network::new(512, 64);
+    let mut net = network::Network::new(256, 63);
 
     // 从节点 0 作为入口，插入 1000000 条，查询 10 次
     // Use node 0 as the entry point, insert 1,000,000 records, and perform 10 queries
